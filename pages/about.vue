03b99dac68976a4b68ee34dd291072769e1795d0
kanD-kaito <rkaito17bb@gmail.com>
2019-11-08 19:59:32 +0900

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>About Page</h1>
+    <NLink to="/">
+      Top Page
+    </NLink>
+     <br>
+    <NLink to="/sample">
+      Sample Page
+    </NLink>
+  </div>
+</template>
+
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>hello world</h1>
+    <NLink to="/about">
+      About Page
+    </NLink>
+    <br>
+    <NLink to="/sample">
+      Sample Page
+    </NLink>
+  </div>
+  
+</template>
+
+ <NLink to="/sample">
+      Sample Page
+    </NLink>
+  </div>
+</template>
+<script>
+export default {
+  head: {
+    title: 'Home page'
+  }
+}
+</script>
